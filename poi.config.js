83e5de4b1c2210edec22d53742f9377defa584d5
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-16 10:05:04 -0300

    Added index.html, sass loader and eslint plugins

--- poi.config.js
+++ poi.config.js
@@ -1,14 +1,34 @@
 const OfflinePlugin = require('offline-plugin')
 
-module.exports = options => ({
-  webpack(config) {
-    if (options.mode === 'production') {
+module.exports = {
+  cssModules: true,
+  extractCSS: false,
+  autoprefixer: {
+    browsers: ['> 5%']
+  },
+  html: {
+    template: 'src/index.html'
+  },
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.{scss,sass}$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'sass-loader'
+      }]
+    })
+
+    if (config.mode === 'production') {
       config.plugins.push(new OfflinePlugin({
         ServiceWorker: {
           events: true
         }
       }))
     }
+
     return config
   }
-})
+}
--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,14 @@
   }
 </script>
 
-<style>
-body {
-  background-color: #f9f9f9;
-  margin: 0;
-  font: 14px/1.4 -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-}
+<style lang="sass">
+html
+  font-size: 14px
+
+body
+  background-color: #f9f9f9
+  font-size: 1rem
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif
+  margin: 0
+  padding: 0
 </style>
